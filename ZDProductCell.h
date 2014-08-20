d38b64dc13d45b98961c8c5529acbe704d7998fb
chen jian <356960930@qq.com>
2014-08-20 16:46:05 -0700

    产品界面
    还需要提供相关文案，以填充整个界面。

---
+++ ZDProductCell.h
@@ -0,0 +1,14 @@
+//
+//  ZDProductCell.h
+//  CRMManager
+//
+//  Created by apple on 14-8-20.
+//  Copyright (c) 2014年 peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDProductCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView* imageView;
+@property (weak, nonatomic) IBOutlet UILabel* label;
+@end
