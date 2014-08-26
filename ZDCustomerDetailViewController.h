63ceb8cdf4a27b32f0fc0a27b835be6312228ceb
chen jian <356960930@qq.com>
2014-08-26 14:01:49 -0700

    客户tab界面

---
+++ ZDCustomerDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZDCustomerDetailViewController.h
+//  CRMManager
+//
+//  Created by apple on 14-8-26.
+//  Copyright (c) 2014年 peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDCustomerDetailViewController : UIViewController
+
+@end
---
+++ ZDCustomerRecordListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZDCustomerRecordListViewController.h
+//  CRMManager
+//
+//  Created by apple on 14-8-26.
+//  Copyright (c) 2014年 peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZDCustomerRecordListViewController : UIViewController
+
+@end
